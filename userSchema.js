8d71d5c7ba393e4e3f879b00f6fcc84e6dba4774
Khushi-084 <122378361+Khushi-084@users.noreply.github.com>
2025-04-18 12:03:14 +0530

    Add files via upload

---
+++ userSchema.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const userSchema = new mongoose.Schema({
+    email:String,
+    usertype:String,
+    cart:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"product"
+        }
+    ],
+    wishlist:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"product"
+        }
+    ],
+    
+    isLike:Boolean
+}, {timestamps:true})
+
+userSchema.plugin(passportLocalMongoose);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
